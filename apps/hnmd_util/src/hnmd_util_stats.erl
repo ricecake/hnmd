4feafbe1c1a1e6b84e1f051b5a74c284158af25d
Sebastian Green-Husted <ricecake@tfm.nu>
2017-01-24 22:40:05 -0500

    add more details to windowless percentile function

--- apps/hnmd_util/src/hnmd_util_stats.erl
+++ apps/hnmd_util/src/hnmd_util_stats.erl
@@ -12,10 +12,20 @@
 
 new(Percentile) -> new(Percentile, []).
 
-new(Percentile, _Opts) -> #hnmd_moving_percentile{ percentile=Percentile }.
+new(Percentile, Opts) when Percentile > 0 andalso Percentile =< 1 ->
+	#hnmd_moving_percentile{ percentile=Percentile, last=undefined, pval=proplists:get_value(pval, Opts, 0.005) }.
 
-get(_Stat) -> ok.
+get(#hnmd_moving_percentile{ last=Value }) -> Value.
 
-add(_Value, Stat) -> Stat.
+add(Value, Stat) -> update_stat(Value, Stat).
 
-update(Value, Stat) -> {Value, Stat}.
+update(Value, Stat) ->
+	NewStat = #hnmd_moving_percentile{ last=NewValue } = update_stat(Value, Stat),
+	{NewValue, NewStat}.
+
+update_stat(Value, #hnmd_moving_percentile{ last=undefined } = Stat) -> Stat#hnmd_moving_percentile{ last=Value };
+update_stat(Value, #hnmd_moving_percentile{ last=Last } = Stat) when Value == Last -> Stat;
+update_stat(Value, #hnmd_moving_percentile{ last=Last, percentile=Perc, pval=Pval } = Stat) when Value <  Last ->
+	Stat#hnmd_moving_percentile{ last= (Last - (Pval/Perc)) };
+update_stat(Value, #hnmd_moving_percentile{ last=Last, percentile=Perc, pval=Pval } = Stat) when Value >  Last ->
+	Stat#hnmd_moving_percentile{ last= (Last + (Pval/(1-Perc))) }.

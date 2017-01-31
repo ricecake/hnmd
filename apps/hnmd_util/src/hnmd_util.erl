11b77739badf8c53595d3637b5991a8fd55eee19
Sebastian Green-Husted <ricecake@tfm.nu>
2017-01-30 22:08:32 -0500

    Update hnmd_util.erl

--- apps/hnmd_util/src/hnmd_util.erl
+++ apps/hnmd_util/src/hnmd_util.erl
@@ -7,6 +7,13 @@
 %% API functions
 %%====================================================================
 
+to_lower(String) when is_list(String) -> list_to_binary(string:to_lower(String));
+to_lower(String) when is_binary(String) ->
+	<< <<(do_lower(Char)):8>> || <<Char:8>> <= String >>.
+
+do_lower(Char) when Char >= 65 andalso Char =< 90 -> Char + 32;
+do_lower(Char) -> Char.
+
 
 %%====================================================================
 %% Internal functions
